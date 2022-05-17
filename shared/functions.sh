1e325f6ae2a90bb74262b59da065e2eee735f09f
Peter Boling <peter.boling@gmail.com>
2022-05-17 23:34:42 +0700

    ✨ elasticsrv function

--- shared/functions.sh
+++ shared/functions.sh
@@ -4,12 +4,12 @@ function cban { cd ~/src/ButterBan; }
 function cdot { cd ~/src/dotfiles; }
 function ch { 
     local project="${1:-railsbling}"
-    cd ~/src/hugo/$1; }
+    cd ~/src/hugo/$project; }
 function co1 { cd ~/src/my/oauth-ruby; }
 function co2 { cd ~/src/my/oauth2; }
 function crubo { 
     local project="${1:-rubocop-lts}"
-    cd ~/src/rubocop-lts/$1; }
+    cd ~/src/rubocop-lts/$project; }
 function cr { 
     local project="${1:-api}"
     cd ~/src/rivi/$project; }
@@ -19,9 +19,14 @@ function cud {
 function psqlsrv {
     local version="${1:-12.8}"
     local action="${2:-start}"
-    ~/.asdf/installs/postgres/$1/bin/pg_ctl -D ~/.asdf/installs/postgres/$1/data -l logfile $2
+    ~/.asdf/installs/postgres/$version/bin/pg_ctl -D ~/.asdf/installs/postgres/$version/data -l logfile $action
 }
 function mongosrv {
     local version="${1:-3.2.9}"
-    ~/.asdf/installs/mongodb/$1/bin/mongod --dbpath ~/.asdf/installs/mongodb/$1/data --port $2
+    local port="${2:-27017}"
+    ~/.asdf/installs/mongodb/$version/bin/mongod --dbpath ~/.asdf/installs/mongodb/$version/data --port $port
+}
+function elasticsrv {
+    local version="${1:-6.8.21}"
+    ~/.asdf/installs/elasticsearch/$version/bin/elasticsearch
 }
