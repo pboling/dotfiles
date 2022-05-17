04677aedea3ac045e90883a1ca0dfcf7a0e3efda
Peter Boling <peter.boling@gmail.com>
2022-05-17 16:17:24 +0700

    ✨ New cd functions

--- shared/functions.sh
+++ shared/functions.sh
@@ -1,4 +1,18 @@
 # changing directory to code project
 function ck { cd ~/Documents/GitHub/$1; }
-function cs { cd ~/Documents/src/$1; }
-function cr { cd ~/src/rivi/$1; }
+function cban { cd ~/src/ButterBan; }
+function cdot { cd ~/src/dotfiles; }
+function ch { 
+    local project="${1:-railsbling}"
+    cd ~/src/hugo/$1; }
+function co1 { cd ~/src/my/oauth-ruby; }
+function co2 { cd ~/src/my/oauth2; }
+function crubo { 
+    local project="${1:-rubocop-lts}"
+    cd ~/src/rubocop-lts/$1; }
+function cr { 
+    local project="${1:-api}"
+    cd ~/src/rivi/$project; }
+function cud {
+    local project="${1:-api}"
+    cd ~/src/uds/$project; }
