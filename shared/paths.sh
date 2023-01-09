83b7a866e8c3361491a74d903169b5dbaec891b9
Peter Boling <peter.boling@gmail.com>
2023-01-09 16:29:25 -0700

    Add gnubin to PATH

--- shared/paths.sh
+++ shared/paths.sh
@@ -7,7 +7,7 @@ export PATH="$HOME/bin:$HOME/.bin:$HOME/.local/bin:$PATH"
 export HOMEBREW_PREFIX="/usr/local/Homebrew";
 export HOMEBREW_CELLAR="/usr/local/Cellar/";
 export HOMEBREW_REPOSITORY="/usr/local/Homebrew";
-export PATH="/usr/local/Homebrew/bin:/usr/local/Homebrew/sbin${PATH+:$PATH}";
+export PATH="/opt/homebrew/opt/grep/libexec/gnubin:/usr/local/Homebrew/bin:/usr/local/Homebrew/sbin${PATH+:$PATH}";
 export MANPATH="/usr/local/Homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/usr/local/Homebrew/share/info:${INFOPATH:-}";
 
