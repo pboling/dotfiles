c776166d397e2a96da45b695737ae1d4fa0f425f
Peter Boling <peter.boling@gmail.com>
2023-01-10 16:13:28 -0700

    🐛 Fix asdf

--- shared/asdf.sh
+++ shared/asdf.sh
@@ -2,5 +2,5 @@
 [[ -s "$HOME/.shared/brew.sh" ]] && source "$HOME/.shared/brew.sh"
 
 # Setup asdf version manager
-ASDF_HOME=$(brew --prefix asdf)
-[[ -f "$ASDF_HOME/asdf.sh" ]] && source "$ASDF_HOME/asdf.sh"
+export ASDF_HOME=$(brew --prefix asdf)
+[[ -f "$ASDF_HOME/libexec/asdf.sh" ]] && source "$ASDF_HOME/libexec/asdf.sh"
